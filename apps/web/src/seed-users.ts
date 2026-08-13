3ddfef74e610da490022b186011502b2117078ec
drago1520 <141066422+drago1520@users.noreply.github.com>
2026-08-13 21:03:11 +0300

    update seed users

--- apps/web/src/seed-users.ts
+++ apps/web/src/seed-users.ts
@@ -1,25 +1,13 @@
 import { auth } from "@/lib/auth";
-
+//NEVER COMMIT with actual names
 type User = {
   name: string;
   email: string;
   password: string;
-  image?: string | undefined;
-  callbackURL?: string | undefined;
-  rememberMe?: boolean | undefined;
+  image: string;
 }
 
-const password = 'securepassword123',
-  seedUsers: User[] = [
-    { email: 'thomas.steenberg@copenhagen-atomics.com', name: 'Thomas Steenberg', password },
-    { email: 'thomas.jam@copenhagen-atomics.com', name: 'Thomas Jam', password },
-    { email: 'peter.szabom@copenhagen-atomics.com', name: "Peter Szabom", password },
-    { email: 'aslak.stubsgaard@copenhagen-atomics.com', name: 'Aslak Stubsgaard', password },
-    { email: 'mads.lousdal@copenhagen-atomics.com', name: 'Mads Lousdal', password },
-    { email: 'lars.with@copenhagen-atomics.com', name: 'Lars With', password },
-    { email: 'anders.jensen@copenhagen-atomics.com', name: 'Anders Jensen', password },
-    { email: 'bertram.agerholm@copenhagen-atomics.com', name: 'Bertram Agerholm', password }
-  ] //TODO: add to bitwarden for fast access on demo
+const seedUsers: User[] = []
 
 for await (const body of seedUsers)
   auth.api.signUpEmail({ body })
